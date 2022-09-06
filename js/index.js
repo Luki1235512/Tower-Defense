b6d7145bfb0d1c050be67f1bdd7568ea2bf93ae2
Luki1235512 <Luki1235512@gmail.com>
2022-09-06 12:20:56 +0200

    Add Enemy class

--- js/index.js
+++ js/index.js
@@ -9,7 +9,35 @@ context.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image()
 image.onload = () => {
-    context.drawImage(image, 0, 0)
+    animate()
 }
 image.src = 'img/map.png'
+
+class Enemy {
+    constructor({position = {x: 0, y: 0}}) {
+        this.position = position
+        this.width = 50
+        this.height = 50
+    }
+
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += 1
+    }
+}
+
+const enemy = new Enemy({position: {x: 200, y: 400}})
+
+function animate() {
+    requestAnimationFrame(animate)
+
+    context.drawImage(image, 0, 0)
+    enemy.update()
+}
+
 
