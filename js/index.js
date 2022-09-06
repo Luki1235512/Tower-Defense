3e8d6c63d03379ff943f7736200464bce3933fc7
Luki1235512 <Luki1235512@gmail.com>
2022-09-06 12:42:12 +0200

    Detect enemy center instead of top left

--- js/index.js
+++ js/index.js
@@ -19,6 +19,10 @@ class Enemy {
         this.width = 50
         this.height = 50
         this.waypointIndex = 0
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2,
+        }
     }
 
     draw() {
@@ -30,14 +34,18 @@ class Enemy {
         this.draw()
 
         const waypoint = waypoints[this.waypointIndex]
-        const yDistance = waypoint.y - this.position.y
-        const xDistance = waypoint.x - this.position.x
+        const yDistance = waypoint.y - this.center.y
+        const xDistance = waypoint.x - this.center.x
         const angle = Math.atan2(yDistance, xDistance)
         this.position.x += Math.cos(angle)
         this.position.y += Math.sin(angle)
+        this.center = {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height / 2,
+        }
 
-        if (Math.round(this.position.x) === Math.round(waypoint.x) &&
-            Math.round(this.position.y) === Math.round(waypoint.y) &&
+        if (Math.round(this.center.x) === Math.round(waypoint.x) &&
+            Math.round(this.center.y) === Math.round(waypoint.y) &&
             this.waypointIndex < waypoints.length - 1) {
             this.waypointIndex++
         }
