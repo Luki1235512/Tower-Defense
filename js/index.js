e98efa42ecb4c4c989898c8a5639f8afa4b244e6
Luki1235512 <Luki1235512@gmail.com>
2022-09-10 09:10:34 +0200

    Add placing buildings

--- js/index.js
+++ js/index.js
@@ -57,12 +57,27 @@ function animate() {
     placementTiles.forEach(tile => {
         tile.update(mouse)
     })
+
+    buildings.forEach(buildings => {
+        buildings.draw()
+    })
 }
 
 const mouse = {
     x: undefined,
     y: undefined
 }
+
+canvas.addEventListener('click', (event) => {
+    if (activeTile) {
+        buildings.push(new Building({
+            position: {
+                x: activeTile.position.x,
+                y: activeTile.position.y
+            }
+        }))
+    }
+})
 
 window.addEventListener('mousemove', (event) => {
     mouse.x = event.clientX
